9edfccbf50629ed226997728001a4fc85caa1fb8
miranda <ella__rose@outlook.com>
2021-03-30 15:02:29 -0400

    added new html pages

---
+++ RepairShopUI/Controllers/BusinessExpensesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleProject.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbayBusinessUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BusinessExpensesController : Controller
+    {
+        private IEbayBusinessDB ebayDBRecords;
+
+        public BusinessExpensesController(IEbayBusinessDB ebayDBRecords)
+        {
+            this.ebayDBRecords = ebayDBRecords;
+        }
+        public IActionResult BusinessExpenses()
+        {
+            return View("~/Views/HtmlPages/BusinessExpenses.cshtml");
+        }
+    }
+}
---
+++ RepairShopUI/Controllers/WarrantyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleProject.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbayBusinessUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WarrantyController : Controller
+    {
+        private IEbayBusinessDB ebayDBRecords;
+
+        public WarrantyController(IEbayBusinessDB ebayDBRecords)
+        {
+            this.ebayDBRecords = ebayDBRecords;
+        }
+        public IActionResult Warranty()
+        {
+            return View("~/Views/HtmlPages/Warranty.cshtml");
+        }
+    }
+}
