2e40ca79024ad14063a655c9f4e5a78b082077eb
miranda <ella__rose@outlook.com>
2021-04-25 21:48:37 -0400

    added 2 new views Profit dashboard and resolution center

---
+++ EbayBusinessUI/Controllers/ProfitController.cs
@@ -0,0 +1,25 @@
+﻿using EbayBusiness.DB;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbayBusinessUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfitController : Controller
+    {
+        private IEbayBusinessDB ebayDBRecords;
+
+        public ProfitController(IEbayBusinessDB ebayDBRecords)
+        {
+            this.ebayDBRecords = ebayDBRecords;
+        }
+        public IActionResult Profit()
+        {
+            return View("~/Views/HtmlPages/Profit.cshtml");
+        }
+    }
+}
---
+++ EbayBusinessUI/Controllers/ResolutionCenterController.cs
@@ -0,0 +1,25 @@
+﻿using EbayBusiness.DB;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbayBusinessUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResolutionCenterController : Controller
+    {
+        private IEbayBusinessDB ebayDBRecords;
+
+        public ResolutionCenterController(IEbayBusinessDB ebayDBRecords)
+        {
+            this.ebayDBRecords = ebayDBRecords;
+        }
+        public IActionResult ResolutionCenter()
+        {
+            return View("~/Views/HtmlPages/ResolutionCenter.cshtml");
+        }
+    }
+}
